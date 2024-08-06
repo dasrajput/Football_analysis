61980b9c03d17c93b9dc0aae116562e0fe0a8888
Dindayalsingh04 <dindyalsingh2310@gmail.com>
2024-08-06 07:08:29 +0530

    END

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import numpy as np
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator   
-
+from view_transformer import ViewTransformer
+from speed_and_distance_estimator import SpeedAndDistance_Estimator
 
 def main():
     # Read the video
@@ -18,15 +19,26 @@ def main():
                                        read_from_stub = True,
                                        stub_path = 'stubs/track_stubs.pkl')
     
+    # get object positions
+    tracker.add_position_to_tracks(tracks)
+    
     #camera movement estimator
     camera_movement_estimator = CameraMovementEstimator(video_frames[0])
     camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames,
                                                                               read_from_stub = True,
                                                                               stub_path = 'stubs/camera_movement_stub.pkl')
+    camera_movement_estimator.add_adjust_positions_to_tracks(tracks, camera_movement_per_frame)
 
+    # View transformer
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(tracks)
     
     #interpolate ball positions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+
+    # Speed and Distance Estimator
+    speed_and_distance_estimator = SpeedAndDistance_Estimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
     
     # assign player teams
     team_assigner = TeamAssigner()
@@ -54,12 +66,15 @@ def main():
     team_ball_control= np.array(team_ball_control)
 
         
-        
-    
+    #Draw output
+    ##Draw object tracks
     output_video_frames = tracker.draw_annotations(video_frames, tracks, team_ball_control)
 
     #Draw camera movement
     output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames, camera_movement_per_frame)
+
+    #Draw speed and distance
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames, tracks)
 
     # Save the video
     save_video(output_video_frames,'Output_Videos/output_video.avi')
